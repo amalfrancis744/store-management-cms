0dcc7406996b3c922726ffdd8b8803755ff02a0a
amalfrancis744 <amal.francis@dignizant.com>
2025-06-18 15:30:30 +0530

    chore: load environment variables from .env.local in service worker script

--- scripts/replace-env-in-sw.js
+++ scripts/replace-env-in-sw.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+require('dotenv').config({ path: '.env.local' });
+
 const envVariables = {
   '%%NEXT_PUBLIC_FIREBASE_API_KEY%%': process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   '%%NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN%%':
